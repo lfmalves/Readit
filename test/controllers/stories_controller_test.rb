f74abbbba0a9411f46bbb84c51a2023b1cf5e6dc
lfmalves <luis.projensino@gmail.com>
2020-02-07 00:26:12 -0300

    formatting

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get stories_index_url
     assert_response :success
   end
-
 end
