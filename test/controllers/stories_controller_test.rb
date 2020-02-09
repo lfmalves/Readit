97cca98f2d61b8801248dcc34caeabad5114b146
lfmalves <luis.projensino@gmail.com>
2020-02-08 22:08:11 -0300

    feat: tests for the controllers

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -4,7 +4,30 @@ require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get stories_index_url
+    get stories_url
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_story_path
+    assert_response :success
+    assert_template 'new'
+    assert_not_nil assigns(:story)
+  end
+
+  test "new shows new form" do
+    get new_story_path
+    assert_select 'form div', count: 2
+  end
+
+  test 'adds a story' do 
+    assert_difference "Story.count" do 
+      post stories_path, params: {
+        story: {
+          name: 'test story',
+          link: 'http://www.test.com/'
+        }
+      }
+    end
   end
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -3,9 +3,20 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  test "is not valid without a name" do 
-    s = Story.create(name: nil, link: 'http://www.noix.com')
+  test 'is not valid without a name' do
+    s = Story.create(name: nil, link: 'http://www.mejr.com')
     assert s.errors[:name].any?
     refute s.valid?
+  end
+
+  test 'is not valid without a link' do
+    s = Story.create(name: 'Milionário e José Rico', link: nil)
+    assert s.errors[:link].any?
+    refute s.valid?
+  end
+
+  test 'is valid with required attributes' do
+    s = Story.create(name: 'Milionário e José Rico', link: 'http://www.mejr.com')
+    assert s.valid?
   end
 end
