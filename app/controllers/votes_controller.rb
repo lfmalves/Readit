98273440138b0e74502adac903120f0fc78b3f2d
lfmalves <luis.projensino@gmail.com>
2020-02-15 11:35:58 -0300

    feat: css styling for votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class VotesController < ApplicationController
-  def create
-  end
+  def create; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'votes/create'
-  resources :stories do 
+  resources :stories do
     resources :votes
   end
 end
