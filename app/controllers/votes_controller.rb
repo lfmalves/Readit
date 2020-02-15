ef87acde7b5ebe3c1b80c33c50d9bb267927e08f
lfmalves <luis.projensino@gmail.com>
2020-02-15 11:44:54 -0300

    feat: response format for votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,5 +4,9 @@ class VotesController < ApplicationController
   def create
     @story = Story.find(params[:story_id])
     @story.votes.create
+    respond_to do |format|
+      format.html {redirect_to @story, notice: 'You voted for it!'}
+      format.js {}
+    end
   end
 end
