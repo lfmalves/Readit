24e2f19dd88aaf8d51792ce213d9221a53676c71
lfmalves <luis.projensino@gmail.com>
2020-02-16 09:51:29 -0300

    feat: limiting the number of votes to be displayed

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,9 @@
 
 class Story < ApplicationRecord
   validates :name, :link, presence: true
-  has_many :votes
+  has_many :votes do 
+    order('id DESC').limit(3)
+  end
 
   def to_param
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
