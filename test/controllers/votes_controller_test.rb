a00d245d8a894b6eef70ac1a7c145dc6bf5f40f8
lfmalves <luis.projensino@gmail.com>
2020-02-22 14:00:01 -0300

    test: redirect after vote with http post

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -13,4 +13,9 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
     post story_votes_path(stories(:two)), xhr: true
     assert_response :success
   end
+
+  test 'redirect after vote with http post' do
+    post story_votes_path(stories(:two))
+    assert_redirected_to story_path(stories(:two))
+  end
 end
