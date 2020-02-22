c32776db1da1d04bff5dfbf0fad05f9481dd453e
lfmalves <luis.projensino@gmail.com>
2020-02-22 13:56:06 -0300

    test: create vote with ajax

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -8,4 +8,9 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
       post story_votes_path(stories(:two))
     end
   end
+
+  test 'create vote with ajax' do
+    post story_votes_path(stories(:two)), xhr: true
+    assert_response :success
+  end
 end
