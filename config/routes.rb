9f68d65f5854a6ce0445bff763d81b36e0d3ef34
lfmalves <luis.projensino@gmail.com>
2020-02-22 16:17:57 -0300

    feat: resource session for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   get 'votes/create'
   resources :stories do
     resources :votes
   end
+  resource :session
 end
