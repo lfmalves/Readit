286de28f9018dc1cbbc074655a5276476ac8bcc1
lfmalves <luis.projensino@gmail.com>
2020-02-22 18:10:15 -0300

    feat: create action for the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @current_user = User.find_by(email: params[:email])
+    if @current_user && @current_user.authenticate(params[:password])
+      session[:user_id] = @current_user.id
+      redirect_to stories_path
+    else
+      render action: 'new'
+    end
+  end
 
   def destroy; end
 end
